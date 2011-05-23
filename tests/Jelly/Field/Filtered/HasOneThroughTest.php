72cb1c9b113f478c8867807055a02c987d27304a
Marcus Cobden <git@marcuscobden.co.uk>
2011-05-23 22:29:50 +0100

    Stubs for future tests.

---
+++ tests/Jelly/Field/Filtered/HasOneThroughTest.php
@@ -0,0 +1,49 @@
+<?php
+
+Jelly_Test::bootstrap();
+
+/**
+ * Tests filtered HasOneThrough relationships
+ *
+ * @group jelly
+ * @group jelly.field
+ * @group jelly.field.filtered
+ * @group jelly.field.filtered.hasOneThrough
+ */
+Class Jelly_Field_Filtered_HasOneThroughTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testFilterProvider()
+	{
+		return array(
+			// No Filtering. Should function as vanilla HasOne
+			// TODO
+			// Filtering on through table
+			// array(1, FALSE, 'current_holder'),
+			// array(2,     2, 'current_holder'),
+			// array(3,     2, 'current_holder'),
+			// array(4, FALSE, 'current_holder'),
+			// Filtering on target model
+			
+		);
+	}
+	
+	/**
+	 * @dataProvider testFilterProvider
+	 */
+	public function testFilter($book_id, $member_id, $field)
+	{
+		$book = Jelly::query('book', $member_id)->select();
+		$member = $book->$field;
+		
+		if ($member_id === FALSE)
+		{
+			$this->assertEquals(FALSE, $member->loaded());
+		}
+		else
+		{
+			$this->assertEquals($member_id, $member->id());
+		}
+		
+	}
+}
---
+++ tests/Jelly/Field/HasOneThroughTest.php
@@ -0,0 +1,15 @@
+<?php
+
+Jelly_Test::bootstrap();
+
+/**
+ * Tests filtered HasOneThrough relationships
+ *
+ * @group jelly
+ * @group jelly.field
+ * @group jelly.field.hasOneThrough
+ */
+Class Jelly_Field_HasOneThroughTest extends PHPUnit_Framework_TestCase
+{
+
+}
